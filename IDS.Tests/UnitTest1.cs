290691c86d47941e8d903090a087785b875a4db0
Valugs <valentin.1999@mail.ru>
2021-11-23 02:00:25 +0300

    Add project files.

---
+++ IDS.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace IDS.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
