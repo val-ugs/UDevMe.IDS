3bf6702059cfc765c9f0c8ea24cde40e48dac08b
Valugs <valentin.1999@mail.ru>
2022-03-16 18:50:48 +0300

    refactor(TrafficData): Changed class properties

--- IDS.Domain/Models/TrafficData.cs
+++ IDS.Domain/Models/TrafficData.cs
@@ -6,13 +6,26 @@ using System.Threading.Tasks;
 
 namespace IDS.Domain.Models
 {
+    public class Sample
+    {
+        public Sample() { }
+
+        public Sample(List<double> features, int label)
+        {
+            Features = features;
+            Label = label;
+        }
+        public List<double> Features { get; set; }
+        public int Label { get; set; }
+    }
+
     public class TrafficData
     {
         public TrafficData()
         {
-            Features = new List<List<double>>();
+            Samples = new List<Sample>();
         }
 
-        public List<List<double>> Features { get; set; }
+        public List<Sample> Samples { get; set; }
     }
 }
