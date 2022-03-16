5cdb933118af1a685d7c433677edee8d53656421
Valugs <valentin.1999@mail.ru>
2022-03-16 22:27:42 +0300

    feat(INormalizeFeaturesService): add an interface to the NormalizeFeaturesService class

---
+++ IDS.Domain/Abstractions/INormalizeFeaturesService.cs
@@ -0,0 +1,15 @@
+﻿using IDS.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDS.Domain.Abstractions
+{
+    public interface INormalizeFeaturesService
+    {
+        public List<Sample> NormalizeTrainSamples(List<Sample> samples, double min, double max);
+        public List<Sample> NormalizeTestSamples(List<Sample> samples);
+    }
+}
