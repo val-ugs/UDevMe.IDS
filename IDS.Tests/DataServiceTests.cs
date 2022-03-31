e09830ebab7581c52f6de3b87882aa14b5cf389e
Valugs <valentin.1999@mail.ru>
2022-03-31 15:36:34 +0300

    test: add tests for the DataServiceTests

---
+++ IDS.Tests/DataServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using IDS.BusinessLogic.Services;
+using IDS.DataAccess.CSV;
+using IDS.DataAccess.PCAP;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDS.Tests
+{
+    public class DataServiceTests
+    {
+        [Test]
+        public void GetData_Csv_ShouldReturnTrue()
+        {
+            // arrange
+            string csvFile = "UNSW_NB15_training-set.csv";
+
+            DataAccess.CSV.DataRepository csvDataRepository = new DataAccess.CSV.DataRepository(
+                new CsvSettings("..\\..\\..\\..\\IDS.DataAccess.CSV\\Data", ',')
+            );
+            DataService dataService = new DataService(csvDataRepository);
+
+            List<string[]> data = dataService.GetData(csvFile, hasHeaderRow: true);
+
+            // assert
+            Assert.IsTrue(data != null);
+        }
+
+        [Test]
+        public void GetData_Pcap_ShouldReturnTrue()
+        {
+            // arrange
+            string pcapFile = "MyTraffic.pcapng";
+
+            DataAccess.PCAP.DataRepository csvDataRepository = new DataAccess.PCAP.DataRepository(
+                "..\\..\\..\\..\\IDS.DataAccess.PCAP\\Data"
+            );
+            DataService dataService = new DataService(csvDataRepository);
+
+            List<string[]> data = dataService.GetData(pcapFile);
+
+            // assert
+            Assert.IsTrue(data != null);
+        }
+    }
+}
